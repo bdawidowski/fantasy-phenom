ee24eb35993f7f063b90d19b3c1fd9f892ed18d3
teamdawi <brett@teamdawi.com>
2017-12-14 23:54:19 -0500

    Overhaul of Source Code of News Page (View and Controllers)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,5 +14,16 @@ class PagesController < ApplicationController
 
   def news
     @articles = Article.where(:approved => true).order(created_at: :desc).limit(30)
+    @article_newest = @articles.first
+    @articles_even, @articles_odd, @articles_all = []
+    @articles.each_with_index do |article, i|
+      if i != 0 and i % 2 == 0 and i <= 8
+        @articles_even.push(article)
+      elsif i != 0 and i <= 8
+        @articles_odd.push(article)
+      else
+        @articles_all.push(article)
+      end
+    end
   end
 end
