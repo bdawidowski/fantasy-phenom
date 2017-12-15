a680f87dc18019d26b49d3def38242b5f16b3fc9
teamdawi <brett@teamdawi.com>
2017-12-15 00:03:53 -0500

    Small bugs to New News

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,15 +14,17 @@ class PagesController < ApplicationController
 
   def news
     @articles = Article.where(:approved => true).order(created_at: :desc).limit(30)
-    @article_newest = @articles.first
-    @articles_even, @articles_odd, @articles_all = []
+    @article_newest = @articles[0]
+    @articles_even =[]
+    @articles_odd = []
+    @articles_all = []
     @articles.each_with_index do |article, i|
       if i != 0 and i % 2 == 0 and i <= 8
-        @articles_even.push(article)
+        @articles_even << article
       elsif i != 0 and i <= 8
-        @articles_odd.push(article)
+        @articles_odd << article
       else
-        @articles_all.push(article)
+        @articles_all << article
       end
     end
   end
