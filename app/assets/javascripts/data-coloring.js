8a98bdc9230f23f1038f9cb813fa61c3a7d61958
Brett Dawidowski <brettdawidowski@Bretts-MBP.home>
2018-03-28 20:09:01 -0400

    Fixing Coloring Function Javascrip Bug

--- app/assets/javascripts/data-coloring.js
+++ app/assets/javascripts/data-coloring.js
@@ -23,8 +23,8 @@ document.onreadystatechange = function () {
         setColors(pitcher_index_3, [40, 38, 35, 30, 28, 23, 20], true);
         var pitcher_index_4 = document.querySelectorAll('.pitcher-data-4');
         setColors(pitcher_index_4, [35], false);
-        // var pitcher_index_5 = document.getElementsByClassName('pitcher-data-5');
-        // setColors(pitcher_index_5, [], false);
+        var pitcher_index_5 = document.querySelectorAll('.pitcher-data-5');
+        setColors(pitcher_index_5, [1], false);
         var pitcher_index_6 = document.querySelectorAll('.pitcher-data-6');
         setColors(pitcher_index_6, [4.7, 4.4, 4.1, 3.8, 3.5, 3.2, 2.9], true);
 
@@ -40,9 +40,12 @@ document.onreadystatechange = function () {
         setColors(batters_index_11, [35], false);
         var batters_index_12 = document.querySelectorAll('.batter-data-12');
         setColors(batters_index_12,  [27.5, 25, 22, 20, 16, 12.5, 10], true);
-        // var batters_index_13 = document.querySelectorAll('.batter-data-13');
-        // setColors(batters_index_13,  [27.5, 25, 22, 20, 16, 12.5, 10], true);
-
+        var batters_index_13 = document.querySelectorAll('.batter-data-13');
+        setColors(batters_index_13,  [10], true);
+        var batters_index_14 = document.querySelectorAll('.batter-data-14');
+        setColors(batters_index_14,  [10], true);
+        var batters_index_15 = document.querySelectorAll('.batter-data-15');
+        setColors(batters_index_15,  [.29, .3, .31, .32, .34, .37, .4], false);
 
 
 
@@ -57,28 +60,33 @@ document.onreadystatechange = function () {
 
     }
 };
-function setColors(nodes, limits) {
-    console.log(nodes)
+function setColors(nodes, limits, is_greater) {
     for(var i = 0; i < nodes.length; i++){
         var td = nodes[i];
         var td_value =  parseFloat(td.textContent.replace('%', ''));
         if(isNaN(td_value)){
-            var class_name = 'td-color-4';
+            var class_name = 'td-color-na';
         } else {
-            class_name = evalLimits(td_value, limits);
+            class_name = evalLimits(td_value, limits, is_greater);
         }
         td.setAttribute('class', class_name);
-        console.log(td);
     }
 }
 function evalLimits(value, limits, is_greater){
     if(limits.length === 1){
-        if(value < limits[0]){
-            return 'td-color-1';
-        } else if (value === limits[0]){
+        if (is_greater){
+            if (value > limits[0]) {
+                return 'td-color-7';
+            }
             return 'td-color-4';
         } else {
+            if (value < limits[0]) {
+                return 'td-color-1';
+            } else if (value === limits[0]) {
+                return 'td-color-4';
+            }
             return 'td-color-7';
+
         }
     } else{
         for(var i = 0; i < limits.length; i++) {
