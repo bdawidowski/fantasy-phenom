bf49b2aa715b64ab3db0a6b34f8845827b611b34
teamdawi <brett@teamdawi.com>
2018-05-15 20:00:59 -0400

    Added last updated time to the Admin Data View

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,6 +1,7 @@
 class DataController < ApplicationController
   def mlb
     @games = MlbGame.all
+    @updated_at = Time.at(MlbGame.first.updated_at.to_i).in_time_zone("Eastern Time (US & Canada)").strftime("%^B %e, %Y - %I:%M %p EST")
   end
 
 end
